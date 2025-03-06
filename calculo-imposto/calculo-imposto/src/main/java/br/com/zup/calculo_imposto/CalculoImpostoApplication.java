cdc087c917046592206eb41223084dc0c669cffb
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-06 10:16:41 -0300

    commit inicial

---
+++ calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/CalculoImpostoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.calculo_imposto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoImpostoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoImpostoApplication.class, args);
+	}
+
+}
---
+++ calculo-imposto/calculo-imposto/src/test/java/br/com/zup/calculo_imposto/CalculoImpostoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.calculo_imposto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoImpostoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
