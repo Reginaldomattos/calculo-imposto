c35f6d38256de6ab96bee5677dd8568797f97dbb
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-11 14:55:36 -0300

    feat: Implementando o ImpostoService na service

---
+++ calculo-imposto/calculo-imposto/src/test/java/br/com/zup/calculo_imposto/service_test/ImpostoService.java
@@ -0,0 +1,39 @@
+package br.com.zup.calculo_imposto.service_test;
+
+import br.com.zup.calculo_imposto.models.Imposto;
+import br.com.zup.calculo_imposto.repositorys.ImpostoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ImpostoService {
+
+    @Autowired
+    private ImpostoRepository impostoRepository;
+
+    public List<Imposto> ListarImpostos() {
+        return impostoRepository.findAll();
+    }
+
+    public Imposto cadastrarImposto(Imposto imposto) {
+        return impostoRepository.save(imposto);
+    }
+
+    public Imposto obterImposto(Long id) {
+        return impostoRepository.findById(id)
+                .orElseThrow(() -> new ConfigDataResourceNotFoundException("Imposto não encontrado"));
+    }
+
+    public void excluirImposto (Long id) {
+        impostoRepository.deleteById(id);
+    }
+
+    public double calcularImposto(Long tipoImpostoId, double valorBase) {
+
+        Imposto imposto = obterImposto(tipoImpostoId);
+        return valorBase * (imposto.getAliquota() / 100.0);
+    }
+}
