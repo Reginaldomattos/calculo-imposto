2794099ab0de4980ab5e00cd771969eafd24b581
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-11 14:56:23 -0300

    feat: Implementando o ImpostoRepository na Repository

--- calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/repositorys/ImpostoRepository.java
+++ calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/repositorys/ImpostoRepository.java
@@ -1,10 +1,13 @@
 package br.com.zup.calculo_imposto.repositorys;
 
 import br.com.zup.calculo_imposto.models.Imposto;
+import br.com.zup.calculo_imposto.models.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ImpostoRepository extends JpaRepository<Imposto, Long> {
-
+    Optional<Usuario> findByUsername(String username);
 }
