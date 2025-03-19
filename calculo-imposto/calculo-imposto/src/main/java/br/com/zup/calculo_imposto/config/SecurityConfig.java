0535325eba8936954481d932191c8f7c2645f065
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-19 17:07:57 -0300

    feat: implementando o Security Config na config

---
+++ calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package br.com.zup.calculo_imposto.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> {
+                    authorize.requestMatchers(HttpMethod.POST,"/user/register").permitAll();
+
+                    authorize.requestMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+                    authorize.anyRequest().permitAll();
+
+                    //authorize.anyRequest().authenticated();
+                }).httpBasic(Customizer.withDefaults());
+
+
+        return http.build();
+    }
+
+
+   @Bean
+   public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+   }
+
+}
