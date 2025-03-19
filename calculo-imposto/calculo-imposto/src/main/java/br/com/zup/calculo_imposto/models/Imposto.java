2aafacb6873b3cada4e4ab71cedb0834a03acd19
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-19 17:14:10 -0300

    feat: implementando a classe imposto na models

--- calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/models/Imposto.java
+++ calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/models/Imposto.java
@@ -15,5 +15,37 @@ public class Imposto {
     private Long id;
     private String nome;
     private String descricao;
-    private double aliquote;
+    private double aliquota;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getAliquota() {
+        return aliquota;
+    }
+
+    public void setAliquota(double aliquota) {
+        this.aliquota = aliquota;
+    }
 }
