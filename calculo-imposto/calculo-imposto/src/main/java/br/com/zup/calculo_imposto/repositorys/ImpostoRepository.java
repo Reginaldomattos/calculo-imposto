812e61cea292bbf690fd608437d871f7d81edaa0
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-19 17:26:49 -0300

    feat: implementando a interface Imposto Repository na repository

--- calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/repositorys/ImpostoRepository.java
+++ calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/repositorys/ImpostoRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface ImpostoRepository extends JpaRepository<Imposto, Long> {
-    Optional<Usuario> findByUsername(String username);
+    Optional<Imposto> findByNome(String nome);
 }
