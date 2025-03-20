1c267a92f65a5367e514fab462319ea13cb950fe
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-20 10:52:37 -0300

    feat: implementando a classe AdminController na Controller

---
+++ calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/controllers/AdminController.java
@@ -0,0 +1,18 @@
+package br.com.zup.calculo_imposto.controllers;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminController {
+
+    @GetMapping("/Bem Vindo")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public String Bem_Vindo_Admin() {
+        return "Bem-vindo, Administrador!";
+    }
+}
