566048e2c2868be96b65d511a66ea6e43332f6e5
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-25 09:29:59 -0300

    feat: Implementando a RoleInvalidException na exception

---
+++ calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/exceptions/RoleInvalidException.java
@@ -0,0 +1,7 @@
+package br.com.zup.calculo_imposto.exceptions;
+
+public class RoleInvalidException extends RuntimeException{
+    public RoleInvalidException(String message) {
+        super(message);
+    }
+}
