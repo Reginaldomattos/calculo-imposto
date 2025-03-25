339f3bea96607fc89dd6456b037065b502c09cc3
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-25 11:17:24 -0300

    fix: foi corrigido erros na Implementação do JwtUtil na infra

--- calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/Infra/JwtUtil.java
+++ calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/Infra/JwtUtil.java
@@ -1,23 +1,50 @@
 package br.com.zup.calculo_imposto.Infra;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
 
+import java.security.Key;
 import java.util.Date;
 import java.util.List;
 
+@Component
 public class JwtUtil {
 
-    private static final String SECRET_KEY = "secret";
+    private Key SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
-    public static String generateToken(String username) {
-        List<String> permissions = List.of("ROLE_USER", "ROLE_ADMIN");
-
+    public String generateToken(String username, List<String> roles) {
         return Jwts.builder()
                 .setSubject(username)
-                .claim("permissions", permissions)
+                .claim("roles", roles)
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 10)) // horas
-                .signWith(signatureAlgorithm.HS256, SECRET_KEY)
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // horas
+                .signWith(SECRET_KEY)
                 .compact();
     }
+
+    public String extractUserName(String token) {
+        return extractUserName(token, claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, ClaimsResolver<T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.resolve(claims);
+    }
+    public interface ClaimsResolver<T> {
+        T resolve(Claims claims);
+    }
+    public Claims extractAllClaims(String token) {
+        JwtParser parser = Jwts.parser().setSigningKey(SECRET_KEY).build();
+        return parser.parseClaimsJws(token).getBody();
+    }
+    public String getUserNameFromToken(String token) {
+        return extractAllClaims(token).getSubject();
+    }
+    public boolean isTokenExpired(String token) {
+        return extractAllClaims(token).getExpiration().before(new Date());
+    }
+
 }
