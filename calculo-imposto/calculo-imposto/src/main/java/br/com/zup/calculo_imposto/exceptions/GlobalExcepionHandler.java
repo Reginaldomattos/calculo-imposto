4bb55db5745b975cfe0f92cee92031c8d3f4b9f6
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-25 09:27:12 -0300

    feat: Implementando a GlobalExcepionHandler na exception

---
+++ calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/exceptions/GlobalExcepionHandler.java
@@ -0,0 +1,20 @@
+package br.com.zup.calculo_imposto.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExcepionHandler extends RuntimeException{
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String, String>> handleRuntimeException(RuntimeException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
+    }
+}
