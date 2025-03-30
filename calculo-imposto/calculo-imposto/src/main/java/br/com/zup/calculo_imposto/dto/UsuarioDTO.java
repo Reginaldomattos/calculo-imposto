aa1ebb5b1138f3f5a23b32b47cd26f33ba9c892f
[Reginaldomattos] <[reginaldo.mattos@zup.com.br]>
2025-03-30 11:55:56 -0300

    fix: foram feitas correções na UsuarioDTO na dto

--- calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/dto/UsuarioDTO.java
+++ calculo-imposto/calculo-imposto/src/main/java/br/com/zup/calculo_imposto/dto/UsuarioDTO.java
@@ -1,22 +1,23 @@
 package br.com.zup.calculo_imposto.dto;
 
+import br.com.zup.calculo_imposto.models.Role;
 import lombok.Data;
 
 @Data
 public class UsuarioDTO {
 
-    private String UserName;
+    private String userName;
     private String password;
     private Role role;
 
-    public UsuarioDTO(String userName, String password, Role role) {
 
+    public UsuarioDTO() {
+
+    }
+
+    public UsuarioDTO(String userName, String password, Role role) {
         this.userName = userName;
         this.password = password;
         this.role = role;
-
-    }
-    public UsuarioDTO() {
-
     }
 }
